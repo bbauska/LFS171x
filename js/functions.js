11746ef723ad0d02403ce2dbddca3fba656e6836
Brian Bauska <brianbauska@gmail.com>
2022-03-31 15:27:49 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,27 @@
-/** Called from index.html within bbauska/CandysRoof.bauska.site/main.  Playing and learning JS.  */
+/** Called from index.html within bbauska/lfs171x.bauska.org/main.  Playing and learning JS.  */
+<!--------------------------------------------------------->
+<!------------ script function: scrollFunction() ---------->
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 
 function myfunction()   // write shit.
  {   

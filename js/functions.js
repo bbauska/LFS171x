d3af302801c6686e8e03e3b67a03e1408ffbad5b
Brian Bauska <brianbauska@gmail.com>
2023-07-20 08:45:02 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,4 @@
-/** Called from index.html within bbauska/lfs171x.bauska.org/main.  Playing and learning JS.  */
-/* ------------------------------------------------------ */
-/* ------------ script function: scrollFunction() ------- */
+/* functions.js - called from index.html within bbauska/lfs171x.bauska.org/main. */
 
 //Get the button
 var mybutton = document.getElementById("myBtn");
@@ -8,6 +6,7 @@ var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+/* --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ script function: scrollFunction() ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-- */
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -16,14 +15,14 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the table of contents.
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 860;
+  document.documentElement.scrollTop = 860;
 }
 </script>
 
-function myfunction()   // write shit.
+function myfunction()   // write shit for testing.
  {   
 document.write("welcome to Learning from Brian.  Pick it up as I go along.  Love the shit out of git.");  
          } 
